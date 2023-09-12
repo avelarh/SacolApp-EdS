852149f6dda6d1c0eb48a1b7927d79a525b7525d
avelarh <103266751+avelarh@users.noreply.github.com>
2023-09-11 22:28:11 -0300

    feat: tabela de usuarios

---
+++ api/src/domains/users/models/User.js
@@ -0,0 +1,30 @@
+const sequelize = require('../../database');
+const {DataTypes} = require('sequelize');
+
+
+const User = sequelize.define('Users', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    role: {
+        type: DataTypes.ENUM,
+        values: ["admin", "user"],
+        allowNull: false,
+    },
+});
