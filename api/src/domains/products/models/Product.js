39d7ccda5c5279d88d921393bb80927a2b4c17c5
avelarh <103266751+avelarh@users.noreply.github.com>
2023-09-21 16:29:03 -0300

    feat: tabela produtos

---
+++ api/src/domains/products/models/Product.js
@@ -0,0 +1,34 @@
+const sequelize = require('../../../../database/index');
+const {DataTypes} = require('sequelize');
+
+
+const Product = sequelize.define('Products', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+Product.sync({alter: true, force: false})
+  .then(() => {
+    console.log('Product table was (re)created');
+  })
+  .catch((err) => console.log(err));
