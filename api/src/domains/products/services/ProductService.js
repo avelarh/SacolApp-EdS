db8261fbc04e4dd7edb3bedc365ecddeb133f1f2
avelarh <103266751+avelarh@users.noreply.github.com>
2023-09-21 16:33:04 -0300

    feat: product service

---
+++ api/src/domains/products/services/ProductService.js
@@ -0,0 +1,49 @@
+const Product = require('../models/Product');
+
+
+class ProductService {
+    async create(body) {
+        const newProduct = {
+            name: body.name,
+            price: body.price,
+            description: body.description,
+            image: body.image,
+        };
+        await Product.create(newProduct);
+    }
+
+    async getAll() {
+        const products = await Product.findAll({
+            attributes: ['id', 'name', 'price', 'description', 'image'],
+        });
+        if (!products) {
+            throw new QueryError('Não há nenhum produto cadastrado');
+        }
+        return products;
+    }
+
+    async getById(id) {
+        const product = await Product.findByPk(id, {
+            attributes: {
+                exclude: ['createdAt', 'updatedAt'],
+            }
+        });
+        if (!product) {
+            throw new QueryError(`Não há um produto com o ID ${id}!`);
+        }
+        return product;
+    }
+
+    async update(id, body) {
+        const product = await Product.findByPk(id);
+        if (!product) {
+            throw new QueryError(`Não há um produto com o ID ${id}!`);
+        }
+        await product.update(body);
+    }
+
+    async delete(id) {
+        const product = await this.getById(id);
+        await product.destroy();
+    }
+}
