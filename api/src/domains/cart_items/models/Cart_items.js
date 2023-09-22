f66bbfcb773ded62968b3420e47929d6e0b7eb64
Enzo Pinheiro <eppierazolli@gmail.com>
2023-09-22 13:44:45 -0300

    cart_items added

---
+++ api/src/domains/cart_items/models/Cart_items.js
@@ -0,0 +1,28 @@
+/*const sequelize = require('../../../../database/index');
+const {DataTypes} = require('sequelize');
+
+
+const Cart_item = sequelize.define('Cart_item', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    amount: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+
+    },
+    
+});
+
+User.sync({alter: true, force: false})
+  .then(() => {
+    console.log('User table was (re)created');
+  })
+  .catch((err) => console.log(err));
+
+module.exports = User;*/
+
+//VERIFICAR ALTERAÇÕES
